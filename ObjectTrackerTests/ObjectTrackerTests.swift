4fb48d347ded247134ba74601096c955228d7a80
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2024-09-20 16:08:49 -0500

    initial commit

---
+++ ObjectTrackerTests/ObjectTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectTrackerTests.swift
+//  ObjectTrackerTests
+//
+//  Created by Wesley Junkins on 9/20/24.
+//
+
+import Testing
+@testable import ObjectTracker
+
+struct ObjectTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
